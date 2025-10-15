cd6f5be52ad28620d81a928a014df987e70ff3cf
[dyad] <projects.hyper@outlook.com>
2025-10-15 20:32:45 +0100

    [dyad] Created placeholder Games page - wrote 1 file(s)

---
+++ src/pages/Games.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import React from 'react';
+
+const Games = () => {
+  return (
+    <div className="p-4">
+      <h1 className="text-2xl font-bold text-vanta-text-light mb-4">Games</h1>
+      <p className="text-vanta-text-medium">Explore various games here!</p>
+    </div>
+  );
+};
+
+export default Games;
