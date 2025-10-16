a998261ced3fe0c427cbf9b94de14bec01c8b29f
[dyad] <projects.hyper@outlook.com>
2025-10-17 00:37:11 +0100

    [dyad] Removed the call to action section from the home page. - wrote 1 file(s)

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -50,15 +50,6 @@ const Index = () => {
         </Card>
       </section>
 
-      {/* Call to Action */}
-      <section className="text-center bg-gray-800 p-10 rounded-lg shadow-xl">
-        <h3 className="text-4xl font-bold mb-4 text-blue-400">Ready to Start Predicting?</h3>
-        <p className="text-lg text-gray-300 mb-6">Join Dyad today and become part of a thriving community.</p>
-        <Button size="lg" className="bg-purple-600 hover:bg-purple-700 text-white text-lg px-8 py-4 rounded-full shadow-lg">
-          Sign Up Free
-        </Button>
-      </section>
-
       {/* Images Section */}
       <div className="absolute top-1/2 right-8 transform -translate-y-1/2 flex flex-col gap-4">
         <img
