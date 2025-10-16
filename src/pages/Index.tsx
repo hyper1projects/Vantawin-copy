f1728087ad97feac583b47bcc607865d0b166b04
[dyad] <projects.hyper@outlook.com>
2025-10-17 00:57:51 +0100

    [dyad] Making the leaderboard image clickable - wrote 1 file(s)

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,15 +1,18 @@
 "use client";
 
 import React from 'react';
+import { Link } from 'react-router-dom'; // Import Link
 
 const Index = () => {
   return (
     <div className="w-full h-full flex flex-col items-center justify-center gap-4 p-4">
-      <img
-        src="/images/Group 1000005755.png"
-        alt="Leaderboard"
-        className="max-w-full h-auto"
-      />
+      <Link to="/leaderboard"> {/* Wrap the image in a Link */}
+        <img
+          src="/images/Group 1000005755.png"
+          alt="Leaderboard"
+          className="max-w-full h-auto"
+        />
+      </Link>
       <img
         src="/images/Group 1000005756.png"
         alt="Prediction Card"
