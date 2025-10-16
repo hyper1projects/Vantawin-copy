89f9419d3ef1b916171c30faad42e6fb389d1d5e
[dyad] <projects.hyper@outlook.com>
2025-10-16 22:11:48 +0100

    [dyad] Changed search bar background color to #053256 - wrote 1 file(s)

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -15,9 +15,9 @@ const SearchInput: React.FC<SearchInputProps> = ({ placeholder = "Search", value
         placeholder={placeholder}
         value={value}
         onChange={onChange}
-        className="pl-10 pr-4 py-2 rounded-[14px] bg-vanta-neon-blue text-vanta-blue-dark placeholder-vanta-blue-dark focus:outline-none focus:ring-2 focus:ring-vanta-neon-blue w-64 text-base font-outfit"
+        className="pl-10 pr-4 py-2 rounded-[14px] bg-[#053256] text-white placeholder-white focus:outline-none focus:ring-2 focus:ring-vanta-neon-blue w-64 text-base font-outfit"
       />
-      <Search className="absolute left-3 text-vanta-blue-dark" size={20} />
+      <Search className="absolute left-3 text-white" size={20} />
     </div>
   );
 };
