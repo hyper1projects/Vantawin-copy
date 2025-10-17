25e2857e7884ec3f0e0ab94dd0b4d92dbd853d66
[dyad] <projects.hyper@outlook.com>
2025-10-17 01:51:15 +0100

    [dyad] Hid the content of the Index page. - wrote 1 file(s)

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,14 +1,12 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import { Button } from '@/components/ui/button';
-import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card';
+// Removed unused imports as the component will now render nothing
+// import { Link } from 'react-router-dom';
+// import { Button } from '@/components/ui/button';
+// import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card';
 
 const Index = () => {
-  return (
-    <div className="relative h-full bg-gradient-to-br from-gray-900 to-black text-white rounded-xl p-8">
-      {/* The Index page is now empty */}
-    </div>
-  );
+  // Returning null will render nothing for this component
+  return null;
 };
 
 export default Index;
