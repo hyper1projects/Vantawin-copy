ff1d6efed57097cc1040cf1a557c1649eaad6d23
[dyad] <projects.hyper@outlook.com>
2025-10-23 08:11:23 +0100

    Reverted all changes back to version af756652505b19b2ffff998f72d415a5b86b6363

--- src/components/TopGamesSection.tsx
+++ src/components/TopGamesSection.tsx
@@ -1,8 +1,8 @@
 "use client";
 
 import React from 'react';
-import Oddscard from './Oddscard'; // Importing the new Oddscard component
-import SectionHeader from './SectionHeader';
+import MatchCard from '@/components/MatchCard';
+import SectionHeader from './SectionHeader'; // Now this import should resolve correctly
 import { TeamLogos } from '@/assets/logos';
 
 const TopGamesSection: React.FC = () => {
@@ -10,50 +10,35 @@ const TopGamesSection: React.FC = () => {
     <div className="w-full py-8 px-4">
       <SectionHeader title="Top Games" bgColor="#0D2C60" className="mb-6" />
       <div className="flex flex-wrap justify-center gap-6 bg-[#0B295B] p-6 rounded-b-xl">
-        <Oddscard
-          matchDate="Today"
-          matchTime="18:00"
-          isLive={false}
+        <MatchCard
+          date="Today"
           team1Logo={TeamLogos.CRY}
           team1Name="Crystal Palace"
           team2Logo={TeamLogos.ASTON}
           team2Name="Aston Villa"
-          option1Label="1"
-          option1Value="1.50"
-          option2Label="X"
-          option2Value="3.20"
-          option3Label="2"
-          option3Value="2.80"
+          option1="1.50"
+          option2="3.20"
+          option3="2.80"
         />
-        <Oddscard
-          matchDate="Tomorrow"
-          matchTime="20:30"
-          isLive={true} {/* Example of a live match */}
+        <MatchCard
+          date="Tomorrow"
           team1Logo={TeamLogos.MANU}
           team1Name="Man. United"
           team2Logo={TeamLogos.LEIC}
           team2Name="Leicester City"
-          option1Label="1"
-          option1Value="1.80"
-          option2Label="X"
-          option2Value="3.00"
-          option3Label="2"
-          option3Value="2.50"
+          option1="1.80"
+          option2="3.00"
+          option3="2.50"
         />
-        <Oddscard
-          matchDate="Upcoming"
-          matchTime="15:00"
-          isLive={false}
+        <MatchCard
+          date="Upcoming"
           team1Logo={TeamLogos.WHU}
           team1Name="West Ham United"
           team2Logo={TeamLogos.CRY}
           team2Name="Crystal Palace"
-          option1Label="1"
-          option1Value="2.10"
-          option2Label="X"
-          option2Value="3.10"
-          option3Label="2"
-          option3Value="2.20"
+          option1="2.10"
+          option2="3.10"
+          option3="2.20"
         />
       </div>
     </div>
