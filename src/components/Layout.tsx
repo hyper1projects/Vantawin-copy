f986ddd356213883e5b6dc8f3355d9977bbf3299
[dyad] <projects.hyper@outlook.com>
2025-10-25 15:04:42 +0100

    [dyad] Removed RightSidebar from Pools, Leaderboard, and Wallet pages - wrote 1 file(s) + extra files edited outside of Dyad

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,22 +1,26 @@
 "use client";
 
 import React from 'react';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom'; // Import useLocation
 import Sidebar from './Sidebar';
 import MainHeader from './MainHeader';
 import RightSidebar from './RightSidebar';
 
 const Layout = () => {
+  const location = useLocation();
+  const excludedPaths = ['/pools', '/leaderboard', '/wallet']; // Paths where RightSidebar should not appear
+  const showRightSidebar = !excludedPaths.includes(location.pathname);
+
   return (
     <div className="flex min-h-screen bg-vanta-blue-dark text-vanta-text-light">
       <Sidebar />
       <MainHeader />
-      <div className="flex-1 ml-60 mt-16 mr-80 mb-4 rounded-xl overflow-hidden">
+      <div className={`flex-1 ml-60 mt-16 ${showRightSidebar ? 'mr-80' : 'mr-4'} mb-4 rounded-xl overflow-hidden`}>
         <div className="p-4">
           <Outlet /> {/* This is where your route components will be rendered */}
         </div>
       </div>
-      <RightSidebar />
+      {showRightSidebar && <RightSidebar />} {/* Conditionally render RightSidebar */}
     </div>
   );
 };
