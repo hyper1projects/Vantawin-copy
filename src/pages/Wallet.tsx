1d5f79512329ea5ece9df76ab497e169c4b0088d
[dyad] <projects.hyper@outlook.com>
2025-10-25 16:59:19 +0100

    [dyad] Removed header and descriptive text from Wallet page - wrote 1 file(s) + extra files edited outside of Dyad

--- src/pages/Wallet.tsx
+++ src/pages/Wallet.tsx
@@ -1,16 +1,16 @@
 "use client";
 
 import React from 'react';
-import SectionHeader from '../components/SectionHeader';
-import WalletOverviewCard from '../components/WalletOverviewCard'; // Import the new component
+// Removed SectionHeader import as it's no longer used
+import WalletOverviewCard from '../components/WalletOverviewCard';
 
 const Wallet = () => {
   return (
     <div className="p-4">
-      <SectionHeader title="Wallet" className="mb-4" textColor="text-vanta-text-light" />
-      <p className="text-vanta-text-medium mb-8">Manage your funds here!</p>
+      {/* Removed SectionHeader component */}
+      {/* Removed "Manage your funds here!" text */}
       
-      {/* Render the new WalletOverviewCard component */}
+      {/* Render the WalletOverviewCard component */}
       <WalletOverviewCard />
     </div>
   );
