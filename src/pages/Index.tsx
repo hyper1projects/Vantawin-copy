77abab560b04a623b24696664c471bca20bb32f3
[dyad] <projects.hyper@outlook.com>
2025-10-25 01:33:58 +0100

    [dyad] Added bottom margin to ImageCarousel. - wrote 1 file(s)

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -3,18 +3,33 @@
 import React from 'react';
 import ImageCarousel from '../components/ImageCarousel';
 import PointsMultiplierSection from '../components/PointsMultiplierSection';
-import TopGamesSection from '../components/TopGamesSection'; // Import the new component
+import LiveGamesSection from '../components/LiveGamesSection';
+import UpcomingGamesSection from '../components/UpcomingGamesSection';
+import SectionHeader from '../components/SectionHeader';
 
-const Index = () => {
+const Index: React.FC = () => {
   return (
-    <div className="p-4">
-      <h1 className="text-xl font-bold text-vanta-text-light mb-4 text-left pl-4">Welcome to VantaWin!</h1>
-      <ImageCarousel />
-      <PointsMultiplierSection />
-      {/* Add the new TopGamesSection component here */}
-      <TopGamesSection />
-      <div className="mt-8 text-center text-vanta-text-light">
-        <p>Explore the features of your new VantaWin application!</p>
+    <div className="min-h-screen bg-vanta-blue text-white p-4 sm:p-6 lg:p-8">
+      <div className="max-w-7xl mx-auto space-y-8">
+        {/* Image Carousel with bottom margin */}
+        <div className="mb-6"> {/* Added mb-6 here */}
+          <ImageCarousel />
+        </div>
+
+        {/* Points Multiplier Section */}
+        <PointsMultiplierSection />
+
+        {/* Live Games Section */}
+        <div className="bg-vanta-blue-medium rounded-lg shadow-sm pb-12">
+          <SectionHeader title="Live Games" className="w-full bg-[#0D2C60] rounded-t-lg" textColor="text-white" />
+          <LiveGamesSection />
+        </div>
+
+        {/* Upcoming Games Section */}
+        <div className="bg-vanta-blue-medium rounded-lg shadow-sm pb-12">
+          <SectionHeader title="Upcoming Games" className="w-full bg-[#0D2C60] rounded-t-lg" textColor="text-white" />
+          <UpcomingGamesSection />
+        </div>
       </div>
     </div>
   );
