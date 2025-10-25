c42b7c136ef7c0af698c5770690d629410ecb146
[dyad] <projects.hyper@outlook.com>
2025-10-25 09:22:04 +0100

    Updated src/pages/Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -3,15 +3,15 @@
 import React from 'react';
 import ImageCarousel from '../components/ImageCarousel';
 import PointsMultiplierSection from '../components/PointsMultiplierSection';
-import TopGamesSection from '../components/TopGamesSection';
-
+import TopGamesSection from '../components/TopGamesSection'; // Import the new component
 const Index = () => {
   return (
-    <div className="p-16">
+    <div className="p-4">
       <h1 className="text-xl font-bold text-vanta-text-light mb-4 text-left pl-4">Welcome to VantaWin!</h1>
-      <ImageCarousel className="mb-8" />
-      <PointsMultiplierSection className="mb-8" />
-      <TopGamesSection className="mb-8" /> {/* Added mb-8 here for consistency */}
+      <ImageCarousel />
+      <PointsMultiplierSection />
+      {/* Add the new TopGamesSection component here */}
+      <TopGamesSection />
       <div className="mt-8 text-center text-vanta-text-light">
         <p>Explore the features of your new VantaWin application!</p>
       </div>
